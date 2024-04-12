29cafc46d1d52e24d9e861fc487bdab924b56d91
Malvina Ciccotosto <tatuciccotosto18@hotmail.com>
2024-04-12 14:01:19 -0300

    cookies2

--- src/app.js
+++ src/app.js
@@ -12,42 +12,73 @@ import fs from "fs";
 import { scheduler } from "timers/promises";
 import { log } from "console";
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import connectDb from "./config/database.js";
+import FileStore from "session-file-store";
+import MongoStore from "connect-mongo";
 
 
-const app = express()
-const PORT = process.env.PORT || 3030
+const app = express();
+const PORT = process.env.PORT || 3030;
+const FileStorage= FileStore(session);
+const DB_URL = 'mongodb://127.0.0.1:27017/ecommerce?retryWrites=true&w=majority'
 
 
-//MiddLewares
+//Settings
 app.set("views", __dirname+"/views")
-app.set("layouts", __dirname + +"/views/layouts/main.handlebars");
 app.set("view engine", "handlebars")
+//Middlewares
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static(__dirname+"/public"))
 app.engine("handlebars", handlebars.engine())
 app.use(cookieParser("hola"))//pasamos una clave
+app.use(session({
+    secret:'Secreto',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(session({
+    //store: new fileStorage({path: "./sessions", ttl: 100, retries:0}),
+    store:MongoStore.create({
+        mongoUrl: DB_URL,
+        ttl: 15
+    }),
+    secret:'hola',
+    resave:false,
+    saveUninitialized:false
+}))
 
 app.listen(3030, () => console.log("Listening on P0RT 3030"))
 
+
+//middleware de auth
+function auth(req, res, next) {
+    if (req.session.user === "pepe" && req.session.admin) {
+        return next ();
+    }
+    res.status(401).send("No estas autorizado");
+}
+
 //Routes
-//app.use("/api/products", productRouter)
 app.get("/", (req, res)=> {
-    res.send("inicio");
+    res.status(200).send("<h1> Clase 00 </h1>");
 });
 
 //rutas cookies
 app.get('/setcookie', (req, res)=> {
-    res.cookie("mi cookie", "Soy el rey del mundo", {maxAge:10000}).send("Set cookie")
-})
+    res
+    .cookie("micookie", "Soy el rey del mundo")
+    .send("Set cookie");
+});
 
 app.get('/getcookie', (req, res)=> {
-    res.sed(req.cookies)
-})
+    res.sed(req.cookies);
+});
 
 app.get('/deletecookie', (req, res)=> {
-    res.clearCookie("micookie").send("Cookie removida")
-})
+    res.clearCookie("micookie").send("Cookie removida");
+});
 
 //cookies seguras
 app.get('/set-signed-cookie', (req, res)=> {
@@ -55,87 +86,52 @@ app.get('/set-signed-cookie', (req, res)=> {
     .cookie("miSignedCookie", "Soy el rey del mundo mundial", {maxAge:100000, signed: true})
     .send("Set signed cookie");
 });
+//ruta session
+app.get('/session', (req, res)=>{
+    if(req.session.counter){
+        req.session.counter++;
+        res.send(`Se ha visitado el sitio" ${req.session.counter} veces`)
+    } else{
+        req.session.counter=1;
+        res.send("Bienvenido/a")
+    }
+})
+
+//iniciar sesion
+app.get("/login", (req, res) => {
+    const { username, password } = req.query;
+    if (username !== "pepe" || password !== "pepepass") {
+        return res.send("Login failed");
+    }
+
+    req.session.user = username;
+    req.session.admin = true,
+    res.send("Login Sucess");
+});
+
+//cerrar sesion
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (!err) {
+            res.send("Saliste de la sesion");
+        } else {
+            res.send({ error: err });
+        }
+    });
+});
+
+//rutas protegidas
+app.get('/privado', auth, (req, res)=>{
+    res.send("Estas en el mejor lugar")
+});
+
 
 app.get('/get-signed-cookie', (req, res)=> {
     res.send(req.signedCookies);
 });
 
 
-
-// const connectMongoDB = async () => {
-// const DB_URL = "mongodb://127.0.0.1:27017/ecommerce?retryWrites=true&w=majority"
-
-// try{
-//     await mongoose.connect(DB_URL) 
-//     
-// }catch(error){
-//     console.error("No se pudo conectar a la DB", error)
-//     process.exit()
-// }
-// }
-
-// connectMongoDB()
-
-
-const environment = async () => { 
-    const DB_URL = 'mongodb://127.0.0.1:27017/ecommerce?retryWrites=true&w=majority'
-    await mongoose.conect(DB_URL)
-    console.log("Conectado a MongoDB!!!");
-
-
-    const PATH = "./src/data/data.json"
-
-    fs.readFile(PATH, "utf-8", (err, data) =>{
-
-        if(err){
-            console.error("Error reading data")
-            return 
-        }
-        
-        const user = JSON.parse(data)
-
-        userModel.insertMany(users)
-        .then(() => console.log("Data inserted!!"))
-        .catch(err => console.log(err))
-        })
-
-
-        let result = await userModel.insertMany({first_name: "Carolina", last_name: "Contreras", email: "caro@gmail.com", gender: "Female", courses: ["65fa2577f6ebc114415c8e51"]})
-console.log(result)
-
-
-    //logica a implementar
-    // const usuarios = await usersModel.find();
-    // const userJson = JSON.stringify(usuarios,null,'\t')
-    // console.log(userJson);
-    // const orders = await orderModel.find()
-    // console.log(orders)
-    const pipeline = [
-        {
-            $match: { size: "medium"},
-        },
-        {
-            $group:{
-                _id:"$name", 
-                total:{$sum: "$quantity"},
-            },
-        },
-        {
-            $sort: {total: 1},
-        },
-    ];
-
-    // const orders = await OrderModel.aggregate(pipeline)
-    // console.log(orders)
-const users= await usersModel.paginate({gender:"Femenino"},{limit:2,page:1})
-console.log(users)
-
-};
-
-
-environment()
-
-
+//Listeners
 const server = app.listen(PORT, ()=>
 console.log("Servier listening on port", PORT)
 );
@@ -144,6 +140,8 @@ const io = new Server(server); //instanciando socket.io
 app.use(express.json())
 
 app.use(studentsRouter)
+
+connectDb();
 
 
 
